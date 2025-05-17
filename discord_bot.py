32f20cd4e50ecbdfd38c953d01b0df43151143ef
rohrow <v-rbindigari@expediagroup.com>
2025-05-18 04:37:22 +0530

    archibald sucks v8

--- discord_bot.py
+++ discord_bot.py
@@ -49,6 +49,7 @@ def is_staff(ctx):
 @commands.check(is_staff)
 @commands.command(aliases=['createThreads', 'ct'])
 async def check_auction_channels(ctx):
+    await ctx.message.delete()
     today = datetime.now().strftime("%m/%d/%Y")
     for channel_name, channel_id in CHANNELS.items():
         channel = bot.get_channel(channel_id)
